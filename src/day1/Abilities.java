6c5318ba71cfd2e206a080d2c792a38db59ab9a0
Hanna Datsko <mega.anya22@gmail.com>
2020-04-24 15:03:28 -0400

    first commit Abilities.java

---
+++ src/day1/Abilities.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class Abilities {
+
+    public static void main(String[] args) {
+
+        boolean Hanna = true;
+
+        if(Hanna==true){
+            System.out.println("Hanna is a good girl");
+
+
+        }else{
+            System.out.println("Hanna is a bad girl");
+        }
+    }
+
+
+
+
+
+
+
+}
+
