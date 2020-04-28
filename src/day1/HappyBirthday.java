5f62f7fd453ea68473bf3f92382999b47c90711f
Hanna Datsko <mega.anya22@gmail.com>
2020-04-28 16:05:21 -0400

    HappyBirthday.java

--- src/day1/HappyBirthday.java
+++ src/day1/HappyBirthday.java
@@ -1,14 +1,12 @@
 package day1;
 
-import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
-
 public class HappyBirthday {
     public static void main(String[] args) {
 
 
         System.out.println("worst ever ");
-        boolean annoing = true;
-        if( annoing ==true){
+        boolean annoying = true;
+        if( annoying ==true){
          System.out.println("We do everything same all the time");
         }
         
